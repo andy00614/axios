e9a31431b3c28ddbcc187ce02054899505efd026
andy00614 <33732843@qq.com>
2019-12-26 10:55:40 +0800

    feat(express): add express
    webpack in express

---
+++ examples/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParse = require('body-parser')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const webpackConfig = require('./webpack.config')
+
+const app = express()
+const compiler = webpack(webpackConfig)
+
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: '/__build__/',
+    stats: {
+      colors: true,
+      chunks: false
+    }
+  })
+)
+
+app.use(webpackHotMiddleware(compiler))
+
+// To serve static files such as images, CSS files, and JavaScript files, use the express
+app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const port = process.env.PORT || 8080
+module.exports = app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
+})
